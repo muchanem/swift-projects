06e893f0e2b932e7fea3c4ae119f1f3a408a5739
Mark Muchane <muchanem@gmail.com>
2016-02-27 13:20:35 -0500

    save

--- Swift Basics.playground/Pages/Types.xcplaygroundpage/Contents.swift
+++ Swift Basics.playground/Pages/Types.xcplaygroundpage/Contents.swift
@@ -3,15 +3,26 @@
 // Concatonation
 let country = "United States of America"
 let state = "North Carolina"
-let city = "Charlotte"
-let space = " "
+let city = "Davidson"
+let street = "Pine Road"
 
-let concatonatedAddress = country + " ," + state + " ," + city
+let concatonatedAddress = country + ", " + state + ", " + city
+
 
 // String Interpolation
 
-let interpolatedAddress = "\(country)"
+let interpolatedAddress = "\(country), \(state), \(city)"
 
+let interpolatedstreetAddress = "\(464) \(street)"
 
+// Code challange
+let name = "Mark"
+let part = "Hi there, "
+let greeting = "\(part) \(name)"
+let stuff = "How are you?"
+let finalGreeting = "\(greeting). \(stuff)"
+//////////////////////////
+///////INTERGERS//////////
+//////////////////////////
 
 //: [Next](@next)
