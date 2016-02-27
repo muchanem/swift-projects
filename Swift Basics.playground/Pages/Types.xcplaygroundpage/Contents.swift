e74ae2f2420c5a6440a6519fd56d011bd1b314b2
Mark Muchane <muchanem@gmail.com>
2016-02-27 12:47:00 -0500

    save

---
+++ Swift Basics.playground/Pages/Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+// Concatonation
+let country = "United States of America"
+let state = "North Carolina"
+let city = "Charlotte"
+let space = " "
+
+let concatonatedAddress = country + " ," + state + " ," + city
+
+// String Interpolation
+
+let interpolatedAddress = "\(country)"
+
+
+
+//: [Next](@next)
